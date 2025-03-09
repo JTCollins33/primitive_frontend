dc3dca9a7305f0ae0bb8800c3b0bd4d3397900de
JTCollins33 <JTCollinsy99@gmail.com>
2025-03-08 22:50:15 -0500

    committing before bed

--- add-pokemon-page/add-pokemon-page.js
+++ add-pokemon-page/add-pokemon-page.js
@@ -1,20 +1,31 @@
+import {addPokemonHeader, addPokemonText, addPokemonImage} from '../data/db.js'
+
 var pokemonName = "";
 var pokemonTitle = "";
 var pokemonDescription = "";
 var pokemonImageLocation="";
 
 submitButton = document.getElementsByClassName("add-pokemon-submit-button")[0]
+console.log(submitButton)
 
 submitButton.addEventListener("click", function(){
+    console.log("submitting")
     const pokemonName = document.getElementById("pokemonName").value;
     const pokemonTitle = document.getElementById("pokemonTitle").value;
     const pokemonDescription = document.getElementById("pokemonDescription").value;
     const pokemonImageLocation = document.getElementById("pokemonImagePicker").value;
 
-    printValues([pokemonName, pokemonTitle, pokemonDescription, pokemonImageLocation])
+    // printValues([pokemonName, pokemonTitle, pokemonDescription, pokemonImageLocation])
+    addValuesToDB(pokemonName, pokemonTitle, pokemonDescription, pokemonImageLocation)
 
     resetValues();
 })
+
+function addValuesToDB(pokemonName, pokemonTitle, pokemonDescription, pokemonImageLocation){
+    addPokemonHeader(pokemonName, pokemonTitle)
+    addPokemonText(pokemonName, pokemonDescription)
+    addPokemonImage(pokemonName, pokemonImageLocation)
+}
 
 function printValues(arr) {
     for (const item of arr){
